112c7c31c36150c7e0536454b12d68b381339a74
Jeff <jff_94@hotmail.com>
2026-03-22 16:48:45 -0300

    Ajustando steps

--- src/test/java/com/jeff/dogapi/steps/BreedsStep.java
+++ src/test/java/com/jeff/dogapi/steps/BreedsStep.java
@@ -1,46 +1,55 @@
 package com.jeff.dogapi.steps;
 
+import com.jeff.dogapi.service.BreedsService;
+import com.jeff.dogapi.validator.BreedsValidator;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
 
 public class BreedsStep {
 
-    @Given("a api {string} está disponível")
-    public void aApiEstaDisponivel(String api_path) {
+    private BreedsValidator breedsValidator = new BreedsValidator();
+    private BreedsService breedsService = new BreedsService();
+    private Response response;
 
-    }
-
-    @When("enviar uma requisição GET para")
-    public void enviarUmaRequisicaoGETPara() {
+    @When("enviar uma requisição GET para listar todas as raças")
+    public void enviarUmaRequisicaoGETParaListarTodasAsRacas() {
+        response = breedsService.listAllBreeds();
     }
 
     @Then("deve retornar status code {int}")
-    public void deveRetornarStatusCode(int arg0) {
+    public void deveRetornarStatusCode(int statusCode) {
+        breedsValidator.validateStatus(response, statusCode);
     }
 
     @And("a resposta deve conter o status {string}")
-    public void aRespostaDeveConterOStatus(String statusCode) {
+    public void aRespostaDeveConterOStatus(String status) {
+        breedsValidator.validateResponseStatus(response, status);
     }
 
     @And("a resposta deve seguir o contrato definido")
     public void aRespostaDeveSeguirOContratoDefinido() {
+        breedsValidator.validateSchema(response, "schemas/breeds-schema.json");
     }
 
     @And("os nomes das raças devem estar em minúsculas")
     public void osNomesDasRacasDevemEstarEmMinusculas() {
+        breedsValidator.validateLowercaseBreeds(response);
     }
 
     @And("todas as sub-raças devem ser listas")
     public void todasAsSubRacasDevemSerListas() {
+        breedsValidator.validateSubBreedsAreLists(response);
     }
 
     @And("o tempo de resposta deve ser menor que {int} segundos")
     public void oTempoDeRespostaDeveSerMenorQueSegundos(int timeOut) {
+        breedsValidator.validateResponseTime(response, timeOut);
     }
 
     @And("a lista de raças deve conter pelo menos um item")
     public void aListaDeRacasDeveConterPeloMenosUmItem() {
+        breedsValidator.validateBreedsNotEmpty(response);
     }
 }
