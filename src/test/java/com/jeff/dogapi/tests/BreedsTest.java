c4978530e87b1257d2fb1a2aba0dd37cdf72abb9
Jeff <jff_94@hotmail.com>
2026-03-22 15:55:11 -0300

    Criando camada test

--- src/test/java/com/jeff/dogapi/tests/BreedsTest.java
+++ src/test/java/com/jeff/dogapi/tests/BreedsTest.java
@@ -1,2 +1,21 @@
-package com.jeff.dogapi.tests;public class BreedsTest {
+package com.jeff.dogapi.tests;
+
+import com.jeff.dogapi.steps.BreedsStep;
+import org.junit.jupiter.api.Test;
+
+public class BreedsTest {
+    private BreedsStep breedsStep = new BreedsStep();
+
+    @Test
+    public void listarTodasAsRacasDeCachorro() {
+        breedsStep.enviarUmaRequisicaoGETParaListarTodasAsRacas();
+        breedsStep.deveRetornarStatusCode(200);
+        breedsStep.aRespostaDeveConterOStatus("");
+        breedsStep.aRespostaDeveSeguirOContratoDefinido();
+        breedsStep.osNomesDasRacasDevemEstarEmMinusculas();
+        breedsStep.todasAsSubRacasDevemSerListas();
+        breedsStep.oTempoDeRespostaDeveSerMenorQueSegundos(2);
+        breedsStep.aListaDeRacasDeveConterPeloMenosUmItem();
+
+    }
 }
