9b513f8e45d84b26427cd65c811222a8de6e0836
Jeff <jff_94@hotmail.com>
2026-03-22 16:20:53 -0300

    Adicionando demais URLS no pathEnum

--- src/test/java/com/jeff/dogapi/utils/PathEnum.java
+++ src/test/java/com/jeff/dogapi/utils/PathEnum.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum PathEnum {
-    GET_ALL_BREEDS("/breeds/list/all");
+    GET_ALL_BREEDS("/breeds/list/all"),
+    GET_BY_BREEDS("/breed/hound/images"),
+    GET_RANDOM_IMAGE("/breeds/image/random");
     private final String path;
 }
