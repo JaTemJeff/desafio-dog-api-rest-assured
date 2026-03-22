d8ea3e93699a4c97b6478f2f34335672dabc9276
Jeff <jff_94@hotmail.com>
2026-03-22 16:46:31 -0300

    Adicionando runner com geração de relatório

---
+++ src/test/java/com/jeff/dogapi/runner/TestRunner.java
@@ -0,0 +1,19 @@
+package com.jeff.dogapi.runner;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.jeff.dogapi.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "camelcase")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html")
+public class TestRunner {
+
+}
